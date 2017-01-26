0001a58fd3195d1a5e16db1bb0444dcc86b7ee70
Anessa Ortner <anessa.ortner@gmail.com>
2017-01-26 11:22:16 -0600

    first draft answers

---
+++ grades.js
@@ -0,0 +1,48 @@
+// A score of 50-60 is an F
+// A score of 61-70 is a D
+// A score of 71-80 is a C
+// A score of 81-90 is a B
+// A score of 91-100 is an A
+
+var scores = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87];
+
+var a = [];
+var b = [];
+var c = [];
+var d = [];
+var f = [];
+
+for (var i = 0; i < scores.length; i++) {
+	if (scores[i] < 61) {
+		f.push(i);
+	} else if (scores[i] > 60 && scores[i] < 71) {
+		d.push(i);
+	} else if (scores[i] > 70 && scores[i] < 81) {
+		c.push(i);
+	} else if (scores[i] > 80 && scores[i] < 91) {
+		b.push(i);
+	} else {
+		a.push(i);
+	}
+}
+
+// How many of each grade??
+console.log("F: ", f.length);
+console.log("D: ", d.length);
+console.log("C: ", c.length);
+console.log("B: ", b.length);
+console.log("A: ", a.length);
+
+// What is the lowest grade?
+var scoresSort = scores.sort(function (a,b) {return a-b});
+console.log("Lowest Number: ", scoresSort[0]);
+
+// What is the highest grade?
+var scoresReverse = scores.sort(function (a,b) {return a-b}).reverse();
+console.log("Highest Number: ", scoresReverse[0]);
+
+
+
+
+
+
